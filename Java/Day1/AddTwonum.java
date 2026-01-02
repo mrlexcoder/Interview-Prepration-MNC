864961a8a2b490f2652cccceb2830f4d5952a086
mrlexcoder <mrlexcoder@gmail.com>
2026-01-02 23:30:17 +0530

    Second commit day 1 addition two num prepration dsa

---
+++ Java/Day1/AddTwonum.java
@@ -0,0 +1,8 @@
+public class AddTwonum{
+    public static void main(String[] args){
+        int num1 = 1;
+        int num2= 32;
+        int sum = num1 + num2;
+        System.out.println(sum);
+    }
+}
