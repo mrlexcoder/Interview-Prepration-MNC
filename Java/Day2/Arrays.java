a7c7132812fc55a38a78467f1073b41784f9d579
mrlexcoder <mrlexcoder@gmail.com>
2026-01-03 09:34:24 -0500

    Day2 arrays ex

--- Java/Day2/Arrays.java
+++ Java/Day2/Arrays.java
@@ -3,5 +3,15 @@ package Java.Day2;
 public class Arrays {
     public static void main(String[] args) {
         System.out.println("Hi");
+
+        int arr[] = new int[5];
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+        arr[3] = 40;
+        arr[4] = 50;    
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
 }
