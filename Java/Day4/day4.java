1deed73598ec3414ea8ec5ec6344bffe0d82801a
mrlexcoder <mrlexcoder@gmail.com>
2026-01-07 22:38:21 +0530

    java string day 4

---
+++ Java/Day4/day4.java
@@ -0,0 +1,12 @@
+public class day4 {
+    public static void main(String[] args){
+        System.out.println("This is Day 4 Java file.");
+    }
+
+
+    int obj1;   
+    final object obj2 = new Object();           
+    public void method1(){  
+        System.out.println("This is method1 in day4 class.");
+    } 
+}
